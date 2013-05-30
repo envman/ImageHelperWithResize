df2d25098bbb0633325641bfe1ae28edaa340e28
robert gill <robertgill@etech.net>
2013-05-30 12:55:45 +0100

    Added image helpers

---
+++ ImageHelperWithResize/Controllers/ImageController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ImageResizer;
+
+namespace ImageHelperWithResize.Controllers
+{
+    public class ImageController : Controller
+    {
+        public ActionResult Image(string image)
+        {
+            var imagesPath = Server.MapPath("/Images");
+            var imageLocation = Path.Combine(imagesPath, image);
+
+            return File(imageLocation, "image/jpeg");
+        }
+
+        public ActionResult ImageResize(string image, int width, int height)
+        {
+            var imagesPath = Server.MapPath("/Images");
+            var imageLocation = Path.Combine(imagesPath, image);
+
+            var settings = new ResizeSettings
+            {
+                Format = "jpg",
+                Mode = FitMode.Stretch,
+                Width = width,
+                Height = height,
+                Scale = ScaleMode.Both
+            };
+
+            var outStream = new MemoryStream();
+            ImageBuilder.Current.Build(System.IO.File.ReadAllBytes(imageLocation), outStream, settings);
+            var resizedImage = outStream.ToArray();
+
+            return File(resizedImage, "image/jpeg");
+        }
+    }
+}
---
+++ ImageHelperWithResize/HtmlHelpers/HtmlHelperExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImageHelperWithResize.HtmlHelpers
+{
+    public static class HtmlHelperExtensions
+    {
+        public static MvcHtmlString Image(this HtmlHelper helper, string image, string alternative)
+        {
+            var urlHelper = new UrlHelper(helper.ViewContext.RequestContext);
+            var url = urlHelper.Action("Image", "Image", new { image });
+
+            var builder = new TagBuilder("img");
+
+            builder.MergeAttribute("src", url);
+            builder.MergeAttribute("alt", alternative);
+
+            return MvcHtmlString.Create(builder.ToString(TagRenderMode.SelfClosing));
+        }
+
+        public static MvcHtmlString Image(this HtmlHelper helper, string image, string alternate, int width, int height)
+        {
+            var urlHelper = new UrlHelper(helper.ViewContext.RequestContext);
+            var url = urlHelper.Action("ImageResize", "Image", new { image, width, height });
+
+            var builder = new TagBuilder("img");
+
+            builder.MergeAttribute("src", url);
+            builder.MergeAttribute("alt", alternate);
+
+            return MvcHtmlString.Create(builder.ToString(TagRenderMode.SelfClosing));
+        }
+    }
+}
